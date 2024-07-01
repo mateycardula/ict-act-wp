73ec8db7d930f8f8dec251d767fbfcfd0280698a
bojanognenov <ognenovbojan@gmail.com>
2024-07-01 15:57:03 +0200

    delete confirmation for meeting

--- ict-act-project/src/main/java/mk/ukim/finki/wp/ictactproject/Web/RegisterController.java
+++ ict-act-project/src/main/java/mk/ukim/finki/wp/ictactproject/Web/RegisterController.java
@@ -51,7 +51,7 @@ public class RegisterController {
     ) {
         try {
             Member member = this.memberService.register(email, password, repeatedPassword, name, surname, institution, PositionType.NEW_USER);
-            emailService.sendVerificationEmail(member, "http://localhost:9090");
+//            emailService.sendVerificationEmail(member, "http://localhost:9090");
             return "redirect:/login";
         } catch (InvalidEmailOrPasswordException | PasswordDoNotMatchException |
                  UsernameAlreadyExistsException |InvalidUsernameException exception) {
